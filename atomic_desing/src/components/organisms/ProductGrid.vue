b435210719a64ecc350e8635b3ad9909ca2983df
JonnerPaz <jonnerpazp@gmail.com>
2026-05-14 14:55:23 -0400

    feat: Resuelve dinámicamente cantidad de productos
    En lugar de tener un arreglo fijo de 10 productos, la idea es que la
    cantidad de productos se determine de acuerdo a la cantidad de productos
    que se tenga en el backend para la futura paginación.

--- atomic_desing/src/components/organisms/ProductGrid.vue
+++ atomic_desing/src/components/organisms/ProductGrid.vue
@@ -1,9 +1,17 @@
 <script setup>
 import Card from "../molecules/Card.vue";
+
+defineProps({
+  products: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
 </script>
 <template>
   <div class="ProductGrid">
-    <div v-for="item in 10" :key="item.id">
+    <div v-for="item in products" :key="item.id">
       <Card :category="item.category" :name="item.name" :price="item.price" />
     </div>
   </div>
