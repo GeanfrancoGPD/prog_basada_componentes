0193f31c755d02e2e3629b492bc145410cd0db38
JonnerPaz <jonnerpazp@gmail.com>
2026-05-14 15:01:03 -0400

    feat: Añade paginación y barra de ordenamiento
    * Cambios importantes:
    * Home:
        * Puede calcular el total de productos (en este caso, 25) y el total de páginas.
        * Se implementa la paginación y el ordenamiento de los productos.
        * Al cambiar de página, devuelve a la parte superior de la pantalla.
    * HomeProduct:
        * Define las propiedades y emites los eventos para la paginación y el ordenamiento.
        * Añade los componentes `ProductToolbar` y `Pagination` en el view y
          les pasa las correspondientes props.

--- atomic_desing/src/components/templates/HomeProduct.vue
+++ atomic_desing/src/components/templates/HomeProduct.vue
@@ -2,7 +2,20 @@
 import Header from "../organisms/Header.vue";
 import Sidebar from "../organisms/Sidebar.vue";
 import ProductGrid from "../organisms/ProductGrid.vue";
+import ProductToolbar from "../molecules/ProductToolbar.vue";
+import Pagination from "../molecules/Pagination.vue";
 import Footer from "../organisms/Footer.vue";
+
+defineProps({
+  products: Array,
+  totalItems: Number,
+  itemsPerPage: Number,
+  currentPage: Number,
+  sortOrder: String,
+  totalPages: Number
+})
+
+const emit = defineEmits(['update:currentPage', 'update:sortOrder', 'page-changed'])
 </script>
 <template>
   <Header />
@@ -10,8 +23,25 @@ import Footer from "../organisms/Footer.vue";
   <div class="page-container">
     <Sidebar />
     <main class="main-content">
-      <h1>Productos</h1>
-      <ProductGrid />
+      <div class="header-content">
+        <h1>Productos</h1>
+      </div>
+      
+      <ProductToolbar 
+        :total-items="totalItems"
+        :items-per-page="itemsPerPage"
+        :current-page="currentPage"
+        :sort-value="sortOrder"
+        @update:sortValue="emit('update:sortOrder', $event)"
+      />
+      
+      <ProductGrid :products="products" />
+      
+      <Pagination 
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @page-changed="emit('page-changed', $event)"
+      />
     </main>
   </div>
   <Footer />
