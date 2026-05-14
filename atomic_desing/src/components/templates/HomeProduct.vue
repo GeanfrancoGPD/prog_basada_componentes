04131b8bd305b0820a1bde7535af5b831e360ffd
JonnerPaz <jonnerpazp@gmail.com>
2026-05-14 13:46:28 -0400

    feat: Integra Sidebar en HomeProduct
    Dentro de los cambios más importantes de este commit:
    * Se añade Sidebar
    * Se encapsula Sidebar y main.main-content dentro de div.page-container,
    para estilizar el layout con flexbox
    * Estilos varios para un mejorar la vista

--- atomic_desing/src/components/templates/HomeProduct.vue
+++ atomic_desing/src/components/templates/HomeProduct.vue
@@ -1,17 +1,36 @@
 <script setup>
 import Header from "../organisms/Header.vue";
+import Sidebar from "../organisms/Sidebar.vue";
 import ProductGrid from "../organisms/ProductGrid.vue";
 import Footer from "../organisms/Footer.vue";
 </script>
 <template>
   <Header />
   <slot name="header"></slot>
-  <h1>Productos</h1>
-  <ProductGrid />
+  <div class="page-container">
+    <Sidebar />
+    <main class="main-content">
+      <h1>Productos</h1>
+      <ProductGrid />
+    </main>
+  </div>
   <Footer />
 </template>
 <style scoped>
+.page-container {
+  display: flex;
+  padding: 30px;
+  align-items: flex-start;
+}
+
+.main-content {
+  flex: 1;
+}
+
 h1 {
   font-size: 30px;
+  margin-top: 0;
+  margin-bottom: 20px;
+  margin-left: 40px;
 }
 </style>
