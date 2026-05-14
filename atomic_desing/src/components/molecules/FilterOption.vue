83dc3e7b1606b628611b9ffe77fb04683f04474c
JonnerPaz <jonnerpazp@gmail.com>
2026-05-14 13:20:17 -0400

    feat: Añade componente FilterOption

---
+++ atomic_desing/src/components/molecules/FilterOption.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Checkbox from '../atoms/Checkbox.vue';
+import Label from '../atoms/Label.vue';
+</script>
+
+<template>
+  <div class="filter-option">
+    <Checkbox />
+    <Label><slot></slot></Label>
+  </div>
+</template>
+
+<style scoped>
+.filter-option {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 12px;
+  cursor: pointer;
+}
+</style>
